c0b3eefcaeee4bd41cfeb36e3839d31f7aab8842
Keith <keithlai124@hotmail.com>
2017-10-20 23:02:09 +1100

    improve my meal plan page

--- meealo.py
+++ meealo.py
@@ -74,9 +74,25 @@ def logout():
 @app.route('/my_meal_plan', methods=['GET'])
 def my_meal_plan():
     # my meal plan only appears if you are logged in, checked in index.html
+
+    # Firstly get the user
     user = User.objects(id=ObjectId(session['session_userid'])).get();
-    return render_template('my_meal_plan.html', user=user)
-    #return render_template('my_meal_plan.html')
+    
+    saved_meal_plans = []
+    for mp in user.meal_plan_ids:     
+        saved_meal_plans.append(Meal_Plan.objects(id=mp.id).get())
+
+    # Try and see if there's a current meal plan
+    try:
+        current_meal_plan = Meal_Plan.objects(id=user.current_meal_plan).get()
+        print(current_meal_plan)    
+        return render_template('my_meal_plan.html', user=user, saved_meal_plans=saved_meal_plans, current_meal_plan=current_meal_plan)
+    except:
+    
+    # if not, don't worry! just don't return it
+        flash('No current meal plan')
+        return render_template('my_meal_plan.html', user=user, saved_meal_plans=saved_meal_plans)
+    
 
 @app.route('/meals/<meal_id>', methods=['GET'])
 def get_meal(meal_id):
