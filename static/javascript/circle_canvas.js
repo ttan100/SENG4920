04988150c22c6cf76ea7a5adf5570014926539be
ttan100 <31614229+ttan100@users.noreply.github.com>
2017-10-20 19:35:48 +1100

    drag and drop event

--- static/javascript/circle_canvas.js
+++ static/javascript/circle_canvas.js
@@ -1,15 +1,18 @@
-<script>
-    var canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext('2d');
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    var radius = 70;
 
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = 'red';
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = '#003300';
-    context.stroke();
-</script>
+// resize the canvas to fill browser window dynamically
+window.addEventListener('resize', draw, false);
+
+function draw() {
+	var c = document.getElementsByClassName("meal_drop_circle");
+	for (var i = 0; i < c.length; i++) {
+		c[i].width = 75;
+		c[i].height = 75;
+		var ctx = c[i].getContext("2d");
+		var centerX = c[i].width / 2;
+		var centerY = c[i].height / 2;
+		ctx.strokeStyle="#333333";
+		ctx.beginPath();
+		ctx.arc(centerX, centerY, 37, 0, 2 * Math.PI, false);
+		ctx.stroke();
+	}
+}
